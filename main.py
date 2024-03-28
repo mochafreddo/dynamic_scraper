cd5e8bd448cd8570aff2ebc59a127de67ab6b183
Jeongwoo Kim <corc411@gmail.com>
2024-03-28 10:07:28 +0900

    feat: Implement web scraping for wanted.co.kr job search
    - Install BeautifulSoup for parsing HTML content
    - Navigate to wanted.co.kr and perform a search for "flutter" jobs
    - Scroll through search results by simulating "End" key presses
    - Extract page content for further processing with BeautifulSoup
    - Remove the screenshot functionality

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 p = sync_playwright().start()
@@ -6,6 +9,30 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://www.google.com")
+page.goto("https://www.wanted.co.kr/")
 
-page.screenshot(path="screenshot.png")
+time.sleep(5)
+
+page.click("button.Aside_searchButton__Xhqq3")
+
+time.sleep(5)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+
+time.sleep(5)
+
+page.keyboard.down("Enter")
+
+time.sleep(5)
+
+page.click("a#search_tab_position")
+
+for x in range(5):
+    time.sleep(5)
+    page.keyboard.down("End")
+
+content = page.content()
+
+p.stop()
+
+soup = BeautifulSoup(content, "html.parser")
